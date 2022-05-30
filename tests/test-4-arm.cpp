171760b970f7b2b0e0ae4e227e87ec637b508c53
Cwaschenko <cpw22@students.uwf.edu>
2022-05-29 21:13:39 -0500

    update

--- tests/test-4-arm.cpp
+++ tests/test-4-arm.cpp
@@ -4,14 +4,7 @@
 
 TEST_CASE("Arm consructor")
 {
-	
-	Arm NewArm(3, 2.0); //number of joints, and constant length of joint
-	NewArm.Display(); // display all joints relative to joint 0
-	NewArm.Display(1); // display joint angle and relative to parent 
-	NewArm.Display(0); // display joint angle only
-	NewArm.Rotate(0, 90);
-	NewArm.Display(); 
-	
+
 }
 
 	
